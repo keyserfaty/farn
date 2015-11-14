d3b262577ab29994198dde46642eeecbc75e7bfe
Belen Curcio <curciobelen@gmail.com>
2015-11-14 20:36:06 -0300

    markers

--- client/www/js/controllers.js
+++ client/www/js/controllers.js
@@ -1,6 +1,6 @@
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+.controller('AppCtrl', function($scope, $ionicModal, $timeout, $http) {
 
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
@@ -55,19 +55,37 @@ angular.module('starter.controllers', [])
 .controller('PlaylistCtrl', function($scope, $stateParams) {
 })
 
-.controller('GeoCtrl', function() {
-  console.log('gola');
-
+.controller('GeoCtrl', function($http) {
   var map = L.map('map');
 
-  L.tileLayer('https://api.tiles.mapbox.com/v4/{mapid}/{z}/{x}/{y}.png?access_token={accesstoken}', {
-    maxZoom: 18,
+
+  var config = {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
       '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
       'Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    mapid: 'mapbox.streets',
-    accesstoken: 'pk.eyJ1Ijoib2tiZWwiLCJhIjoiY2lnbWNjbzQ3MDIxMHVubHp3dGVwbXVnaSJ9.SjPEGzzlgpvcmR_OaziFmw'
-  }).addTo(map);
+    mapid: 'okbel.o5mboocj',
+    accesstoken: 'pk.eyJ1Ijoib2tiZWwiLCJhIjoiY2lnbWNjbzQ3MDIxMHVubHp3dGVwbXVnaSJ9.SjPEGzzlgpvcmR_OaziFmw',
+  };
+
+  L.tileLayer('https://api.tiles.mapbox.com/v4/{mapid}/{z}/{x}/{y}.png?access_token={accesstoken}', config).addTo(map);
+
+  // Markers
+  $http.get('https://api.mapbox.com/v4/' + config.mapid + '/features.json?access_token=' + config.accesstoken)
+  .then(function(res){
+    var geojsonFeature = res.data.features;
+    console.log(res);
+    L.geoJson(geojsonFeature).addTo(map);
+  }, function(){
+    console.log('error getting markers');
+  });
+
+
+  // random red circle
+  var circle = L.circle([-34.605519, -58.37413699999999], 187, {
+      color: 'red',
+      fillColor: '#f03',
+      fillOpacity: 0.5
+    }).addTo(map);
 
   function onLocationFound(e) {
     var radius = e.accuracy / 2;
@@ -86,4 +104,5 @@ angular.module('starter.controllers', [])
   map.on('locationerror', onLocationError);
 
   map.locate({setView: true, maxZoom: 16});
+
 });
