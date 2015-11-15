fc1204f07aa66a2bd7e7b93a15e1673007aade68
keyserfaty <keserfaty@gmail.com>
2015-11-15 01:48:23 -0300

    add services

---
+++ client/www/js/main.services.js
@@ -0,0 +1,35 @@
+'use strict';
+
+angular.module('main.services', [])
+
+.factory('HeaderRestangular', ['Restangular', '$localStorage', function (Restangular, $localStorage) {
+
+  return Restangular.withConfig(function (RestangularConfigurer) {
+    // TODO: save token to localStorage from cb end
+    if ($localStorage.token !== undefined) {
+      RestangularConfigurer.setDefaultHeaders({'Authorization': 'Bearer ' + $localStorage.token });
+    }
+
+  });
+}])
+
+.factory('api', ['HeaderRestangular', '$routeParams', function (HeaderRestangular, $routeParams) {
+
+  return {
+  	places: function () {
+      // '/'
+  		return HeaderRestangular.all('place');
+  	},
+  	place: function () {
+      // '/place/:id'
+  		return HeaderRestangular.one('place', $routeParams._id);
+  	},
+    users: function () {
+      return HeaderRestangular.all('user');
+    },
+    user: function () {
+      return HeaderRestangular.one('user', $routeParams._id);
+    }
+  }
+
+}])
