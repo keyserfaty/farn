632fee0eb6c1329291eaa9b405fd3d6f08a97bc6
keyserfaty <keserfaty@gmail.com>
2015-11-17 22:59:09 -0300

    some get/put/post req to the api from the frontend

--- client/www/js/activity/activity.controller.js
+++ client/www/js/activity/activity.controller.js
@@ -1,34 +1,77 @@
 'use strict';
 
 angular.module('activity.controller', [])
-.controller('ActivityCtrl', function ($scope) {
-	$scope.posts = [
-		{
-			user: 'Hola',
-			date: 'November 15, 2015',
-			thumb: 'img/ionic.png',
-			fullimage: 'img/ionic.png',
-			description: 'description',
-			likes: 12,
-			comments: 12
-		},
-		{
-			user: '2do post',
-			date: 'November 15, 2015',
-			thumb: 'img/ionic.png',
-			fullimage: 'img/ionic.png',
-			description: 'description',
-			likes: 12,
-			comments: 12
-		},
-				{
-			user: '2do post',
-			date: 'November 15, 2015',
-			thumb: 'img/ionic.png',
-			fullimage: 'img/ionic.png',
-			description: 'description',
-			likes: 12,
-			comments: 12
-		}
-	];
-});
+.controller('ActivityCtrl', ['api', '$scope', function (api, $scope) {
+  // GET to '/'
+  api.places().getList()
+  .then(function (places) {
+    $scope.places = places;
+
+    $scope.addPlace = function (name, description, geotag, tag, featured) {
+      let newPlace = {
+        name: name,
+        description: description,
+        geotag: geotag,
+        tag: tag,
+        featured: false
+      };
+
+      // POST to '/'
+      api.places().post(newPlace);
+    };
+
+  });
+
+  // GET to '/place/:id'
+  // 'id' will be read from path where the request is being made
+  api.place().get()
+  .then(function (place) {
+    $scope.addPost = function (place, user, thumb, fullimage, description) {
+      /* 'place' refers to the place where the get is being made
+        will be used to make a PUT to that path
+      */
+
+      let newPost = {
+        user: false,
+        thumb: false,
+        fullimage: false,
+        description: description
+      };
+
+      // PUT to '/place/:id'
+      newPost.put()
+    };
+    
+  });
+
+
+  $scope.posts = [
+    {
+      user: 'Hola',
+      date: 'November 15, 2015',
+      thumb: 'img/ionic.png',
+      fullimage: 'img/ionic.png',
+      description: 'description',
+      likes: 12,
+      comments: 12
+    },
+    {
+      user: '2do post',
+      date: 'November 15, 2015',
+      thumb: 'img/ionic.png',
+      fullimage: 'img/ionic.png',
+      description: 'description',
+      likes: 12,
+      comments: 12
+    },
+        {
+      user: '2do post',
+      date: 'November 15, 2015',
+      thumb: 'img/ionic.png',
+      fullimage: 'img/ionic.png',
+      description: 'description',
+      likes: 12,
+      comments: 12
+    }
+  ];
+}]);
--- client/www/js/main/main.services.js
+++ client/www/js/main/main.services.js
@@ -2,16 +2,17 @@
 
 angular.module('main.services', [])
 
-.factory('HeaderRestangular', ['Restangular', '$localStorage', function (Restangular, $localStorage) {
+// TODO: add header to every request when there is a token present
+// .factory('HeaderRestangular', ['Restangular', '$localStorage', function (Restangular, $localStorage) {
 
-  return Restangular.withConfig(function (RestangularConfigurer) {
-    // TODO: save token to localStorage from cb end
-    if ($localStorage.token !== undefined) {
-      RestangularConfigurer.setDefaultHeaders({'Authorization': 'Bearer ' + $localStorage.token });
-    }
+//   return Restangular.withConfig(function (RestangularConfigurer) {
+//     // TODO: save token to localStorage from cb end
+//     if ($localStorage.token !== undefined) {
+//       RestangularConfigurer.setDefaultHeaders({'Authorization': 'Bearer ' + $localStorage.token });
+//     }
 
-  });
-}])
+//   });
+// }])
 
 .factory('api', ['HeaderRestangular', '$routeParams', function (HeaderRestangular, $routeParams) {
 
